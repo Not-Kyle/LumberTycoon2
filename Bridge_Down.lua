8d6b3a40da4f052946430532c749f3e82b7943bd
CORYU <77679412+Not-Kyle@users.noreply.github.com>
2023-08-26 09:16:02 -0400

    Update Bridge_Down.lua

--- Bridge_Down.lua
+++ Bridge_Down.lua
@@ -2,7 +2,7 @@ for _,v in pairs(workspace.Bridge.VerticalLiftBridge:GetChildren()) do
 	if v.Name == 'Lift' then
 		for c = 1,#v:GetChildren() do
 			if v:GetChildren()[c]:IsA'Part' and v:GetChildren()[c].Material == Enum.Material.Concrete and v:GetChildren()[c].Color == Color3.fromRGB(27,42,53) and v:GetChildren()[c].Transparency == 0 then
-				v:GetChildren()[c].CFrame = CFrame.new(113.6, 9, -883)
+				v:GetChildren()[c].CFrame = CFrame.new(113.6, 6.4, -883)
 			end
 		end
 	end
