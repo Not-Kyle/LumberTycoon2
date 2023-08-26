a82972cdaa0c2f0686412cfff8e88a6938d085f9
CORYU <77679412+Not-Kyle@users.noreply.github.com>
2023-08-26 09:14:59 -0400

    Update Bridge_Down.lua

--- Bridge_Down.lua
+++ Bridge_Down.lua
@@ -1,8 +1,13 @@
-for i,v in pairs(workspace.Bridge.VerticalLiftBridge:GetChildren()) do
-    if v.Name == 'Lift' then
-	v.Base.CFrame = CFrame.new(113.6, 6.4, -883)
-    end
+for _,v in pairs(workspace.Bridge.VerticalLiftBridge:GetChildren()) do
+	if v.Name == 'Lift' then
+		for c = 1,#v:GetChildren() do
+			if v:GetChildren()[c]:IsA'Part' and v:GetChildren()[c].Material == Enum.Material.Concrete and v:GetChildren()[c].Color == Color3.fromRGB(27,42,53) and v:GetChildren()[c].Transparency == 0 then
+				v:GetChildren()[c].CFrame = CFrame.new(113.6, 9, -883)
+			end
+		end
+	end
 end
+	
 for i,v in pairs(workspace.Bridge.VerticalLiftBridge:GetChildren()) do
     if v.Name == 'Weight' then
 	v:Destroy()
